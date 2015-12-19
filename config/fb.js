2ca820448604e7a5c5ffcc33f44f7c88e23c204d
neliabennett <nbennet1@mail.sfsu.edu>
2015-12-19 07:34:32 +0000

    changes

---
+++ config/fb.js
@@ -0,0 +1,6 @@
+// facebook app settings - fb.js
+module.exports = {
+  'appID' : '<your_app_identifier>',
+  'appSecret' : '<your_app_secret>',
+  'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+};
